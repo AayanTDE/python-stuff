7c45ee33462ff4e5c19029476097ca2f03d69fc3
AayanTDE <aayantde@hotmail.com>
2021-07-04 09:13:32 +0500

    error handling for not being owner

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,7 @@
 import discord
 from discord.ext import commands
 #from dislash import *
-from dotenv import load_dotenv
-from os import getenv
-load_dotenv()
-token = getenv("TOKEN")
+from util.config import token
 
 bot = commands.Bot(command_prefix="o ")
 #slash = SlashClient(bot)
@@ -16,12 +13,19 @@ async def on_ready():
     print("Obie trice, real name no gimmicks")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="amamamamamamama"))
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.NotOwner):
+        await ctx.reply("Only my owner can use this command!")
 
+bot.load_extension("jishaku")
 bot.load_extension("repeat")
 bot.load_extension("trolling")
 bot.load_extension("rand")
 bot.load_extension("owo")
 bot.load_extension("rater")
 bot.load_extension("embed")
+bot.load_extension("eval")
+bot.load_extension("skyblock")
 
 bot.run(token)
