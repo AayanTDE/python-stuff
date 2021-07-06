c7f8ed8afcfdc3ace239457ec464ffcf7328edec
AayanTDE <aayantde@hotmail.com>
2021-07-06 17:26:19 +0500

    factors in collection and warts counter now

--- bot/skyblock.py
+++ bot/skyblock.py
@@ -1,5 +1,7 @@
 import requests
 from discord.ext import commands
+from math import log10
+from re import sub
 from util.secret_config import key
 from util.skill import *
 from util.uuid import uuid
@@ -11,6 +13,7 @@ class skyblock(commands.Cog):
     
     @commands.command(name="rates", aliases=["r"])
     async def rates(self, ctx, ign, profile=None):
+        msg = await ctx.send("hi if you are seeing this it Means my owner is being bad at coding")
         mcuuid = uuid(ign)
         if profile is None:
             request = requests.get(f"https://api.slothpixel.me/api/skyblock/profile/{mcuuid}?key={key}")
@@ -46,21 +49,21 @@ class skyblock(commands.Cog):
         else:
             pet_level = 0
         
-#fortune from wart hoe
+# fortune from wart hoe
         hoe_ff = 0
 
         try:
             hoe = r["members"][mcuuid]["inventory"][0]["attributes"]["id"]
         except KeyError:
             await ctx.reply("You must place your hoe in your first hotbar slot!")
-
+    # hoe rarity
         if hoe == "THEORETICAL_HOE_WARTS_1":
             hoe_ff += 10
         elif hoe == "THEORETICAL_HOE_WARTS_2":
             hoe_ff += 25
         elif hoe == "THEORETICAL_HOE_WARTS_3":
             hoe_ff += 50
-        
+    # hoe reforge
         try:
             reforge = r["members"][mcuuid]["inventory"][0]["attributes"]["modifier"]
         except KeyError:
@@ -83,40 +86,62 @@ class skyblock(commands.Cog):
                 hoe_ff += 7
             elif rarity == "common":
                 hoe_ff += 5
-            
+    # harvesting
         try:
             harvesting = r["members"][mcuuid]["inventory"][0]["attributes"]["enchantments"]["harvesting"]
         except KeyError:
             harvesting = 0
         for i in range(harvesting):
             hoe_ff += 12.5
-        
+    # turbo warts
         try:
             turbo_warts = r["members"][mcuuid]["inventory"][0]["attributes"]["enchantments"]["turbo_warts"]
         except KeyError:
             turbo_warts = 0
         for i in range(turbo_warts):
             hoe_ff += 5
-
+    # cultivating
         try:
             cultivating = r["members"][mcuuid]["inventory"][0]["attributes"]["enchantments"]["cultivating"]
         except KeyError:
             cultivating = 0
         for i in range(cultivating):
             hoe_ff += 1
+   # collection analysis
+        if hoe == "THEORETICAL_HOE_WARTS_3":
+            try:
+                wart_collection = r["members"][mcuuid]["collection"]["NETHER_STALK"]
+            except KeyError:
+                wart_collection = 1
 
-        ff = (farming_level * 4) + (anita * 2) + (pet_level * 1.8) + hoe_ff
+            wart_collection = int(log10(wart_collection) + 1) - 4
+
+            hoe_ff += wart_collection * 8
+    # logarithmic counter
+        if hoe in ["THEORETICAL_HOE_WARTS_2", "THEORETICAL_HOE_WARTS_3"]:
+            try:
+                for line in r["members"][mcuuid]["inventory"][0]["lore"]:
+                    if "Counter: " in line:
+                        counter = line
+            except KeyError:
+                counter = 0
+            counter = sub('§[0-9a-fk-or]', '', counter); counter = int(sub('[^-0-9\/]+', '', counter))
+
+            counter = int(log10(counter) + 1) - 4
+            hoe_ff += counter * 16
+
+
+        ff = (farming_level * 4) + (anita * 2) + (pet_level * 1.8) + hoe_ff 
 
         wart_coins = 3 * (2 * (1 + ff/100))
 
 
-        await ctx.reply(f"Total farming fortune: {ff}\n\
+        await msg.edit(content=f"Total farming fortune: {ff}\n\
 Fortune from farming skill: {farming_level * 4}\n\
 Fortune from Anita bonus: {anita * 2}\n\
 Fortune from elephant: {pet_level * 1.8}\n\
 Fortune from wart hoe: {hoe_ff}\n\
-You should be getting {wart_coins} coins from one block of warts!")
-
+You should be getting {wart_coins} coins from one block of warts!\n")
 
 def setup(bot: commands.Bot):
     bot.add_cog(skyblock(bot))
