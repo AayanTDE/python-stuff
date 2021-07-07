99938624b4883215bf9720fc1bd30d46b29b7cee
AayanTDE <aayantde@hotmail.com>
2021-07-07 10:41:50 +0500

    discord.AllowedMentions 😳

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,16 @@ from discord.ext import commands
 #from dislash import *
 from util.secret_config import token
 
-bot = commands.Bot(command_prefix="o ")
+bot = commands.Bot(
+    command_prefix="o ",
+    allowed_mentions=discord.AllowedMentions(
+        users=True,         # Whether to ping individual user @mentions
+        everyone=True,      # Whether to ping @everyone or @here mentions
+        roles=True,         # Whether to ping role @mentions
+        replied_user=False,  # Whether to ping on replies to messages
+    ),
+)
+
 #slash = SlashClient(bot)
 #guilds = [802883640286773269]
 
